bfb5a51c0e4b4be15434cb602bbc54563a0706e7
CommissionGuard <commissionguard1@gmail.com>
2025-06-25 17:06:56 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { cartographer } from "@replit/vite-plugin-cartographer";
-import { runtimeErrorModal } from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    cartographer(),
-    runtimeErrorModal(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./client/src"),
-      "@assets": path.resolve(__dirname, "./attached_assets"),
-    },
+      "@assets": path.resolve(__dirname, "./attached_assets")
+    }
   },
   root: "client",
   build: {
     outDir: "dist",
-    emptyOutDir: true,
+    emptyOutDir: true
   },
   server: {
     host: "0.0.0.0",
-    port: 5000,
-  },
+    port: 5000
+  }
 });
-    emptyOutDir: true,
   },
 });
